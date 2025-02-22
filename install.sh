6f8b9b5d1963266434a66cecd29a8a8142d1561f
rocketop <149897756+rocketop1@users.noreply.github.com>
2025-02-22 23:58:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,24 +26,26 @@ $(tput setaf 6) Please note the egg is forked from PterodactylEgg (This is essen
     "  
 }
 
-forceHibernationPlugins() {
-    mkdir -p plugins/noMemberShutdown
-    
-    # Download plugins
-    cd plugins || exit
+forceStuffs() {
+    mkdir -p plugins
+    cd plugins
+
+    # Remove noMemberShutdown plugin if it exists
+    rm -rf noMemberShutdown IdleServerShutdown-1.3.jar
+
+    # Download hibernation-related plugins
     curl -O https://raw.githubusercontent.com/rocketop1/maxio.egg/main/Bruce.jar
     curl -O https://www.spigotmc.org/resources/spark.57242/download?version=489830
-    curl -O https://cdn.modrinth.com/data/DgUoVPBP/versions/QucVTrXS/IdleServerShutdown-1.3.jar
-    
-    # Download noMemberShutdown config
-    cd noMemberShutdown || exit
-    curl -O https://raw.githubusercontent.com/rocketop1/maxio.egg/main/config.yml
-    
-    # Return to the root directory
-    cd ../../
-    
+    curl -O https://cdn.modrinth.com/data/DgUoVPBP/versions/QucVTrXS/IdleServerShutdown-1.3.jar  # Alternative hibernation
+
+    # Additional hibernation plugins (modify as needed)
+    curl -O https://www.spigotmc.org/resources/hibernate.4441/download?version=506703
+
+    cd ..
+
     # Accept EULA
-    echo "eula=true" > eula.txt
+    echo "eula=
+true" > eula.txt
 }
 
 
